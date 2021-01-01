b70223b8df602172ad27c1176b448d4d338aa374
BuildTools <unconfigured@null.spigotmc.org>
2021-01-01 16:29:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shew/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.shew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        Button btn1 = (Button) findViewById(R.id.btn);
+        btn1.setText("Oh, yea, I’ve been clicked!");
+    }
+}
+
